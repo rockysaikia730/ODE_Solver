34f85dd90e0b4f064627c9c39876e0a055428e70
TheImprovedMuffinMan <andras.horkay@gmail.com>
2025-11-24 09:39:03 +0100

    initial design for reader
    Needs to be revised. Create new file for the struct of table point as well. Maybe place all using ... = ... into one file? they are commonly shared across the different files

--- Codes/Reader.h
+++ Codes/Reader.h
@@ -4,5 +4,35 @@
 
 #ifndef CODES_READER_H
 #define CODES_READER_H
+#include <string>
+#include <functional>
+#include <variant>
 
+template <typename T>
+class Reader{
+protected:
+    using State = std::vector<T>;
+    struct TableEntry {
+        double t;
+        State y;
+        State f;
+    };
+    char sep;
+    std::string filename;
+    using Function = std::function<State(double, const State& )>;
+    using data = std::variant<Function, TableEntry>;
+    virtual bool checkFileName() const;
+
+public:
+    Reader(std::string, char);
+    virtual ~Reader();
+    virtual bool checkFileExist() const;
+    virtual data Read() = 0;
+
+    void setFileName(const std::string &newname) {filename = newname;};
+    void setFileSeparator(const char &newsep) {sep = newsep;};
+    std::string getFileName() const{ return filename;}
+    char getChar() const  { return sep;}
+
+};
 #endif //CODES_READER_H
