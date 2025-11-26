e114458a3485eac9c39991857496a125d78230bf
TheImprovedMuffinMan <andras.horkay@gmail.com>
2025-11-26 11:52:21 +0100

    reader class
    Begun work on reader. Missing subclasses.
    Need to correct coding style and add doxygen comments

--- Codes/Reader.cpp
+++ Codes/Reader.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "Reader.h"
+#include <fstream>
+
+template<typename T>
+bool Reader<T>::checkFileName() const {
+    bool result = false;
+
+    return result;
+}
+
+template<typename T>
+bool Reader<T>::checkFileExist() const {
+    bool result = true;
+    if (!) {
+        throw std::runtime_error("File does not exist");
+    }
+
+    return file.good();
+}
+
+template<typename T>
+Reader<T>::Reader(const std::string &newfile,const char &newsep) {
+    bool result = false;
+
+    result = checkFileName();
+    file.open(newfile);
+    result = checkFileExist();
+
+    if (result) {
+        filename = newfile;
+        sep = newsep;
+    }
+
+}
--- Codes/Reader.h
+++ Codes/Reader.h
@@ -7,6 +7,7 @@
 #include <string>
 #include <functional>
 #include <variant>
+#include <fstream>
 
 template <typename T>
 class Reader{
@@ -19,8 +20,10 @@ protected:
     };
     char sep;
     std::string filename;
+    std::ifstream file;
     using Function = std::function<State(double, const State& )>;
     using data = std::variant<Function, TableEntry>;
+
     virtual bool checkFileName() const;
 
 public:
@@ -29,7 +32,9 @@ public:
     virtual bool checkFileExist() const;
     virtual data Read() = 0;
 
-    void setFileName(const std::string &newname) {filename = newname;};
+    Reader(const std::string &filename,const char &sep);
+
+    void setFile(const std::string &newname);
     void setFileSeparator(const char &newsep) {sep = newsep;};
     std::string getFileName() const{ return filename;}
     char getChar() const  { return sep;}
